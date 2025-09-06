b359344f79bc7dfde651faaeeb69f7491f036b54
juangarcia-A <a24310305@live.ceti.mx>
2025-09-06 00:58:14 +0000

    practica 1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <Foco.hpp>
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main()
 {
-    cout<<"hola mundo"<<endl;
-    return 0;
+    Foco foquito;
+    foquito.Encender();
+    foquito.Encender();
 }
